6c8fa05f73b189da3161f5f3a8a0db6afab22fb4
lucas <348764327@qq.com>
2017-02-20 08:56:34 +0800

    update

---
+++ NFinal.IO/System/ViewBagAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public class ViewBagAttribute:Attribute
+    {
+        public ViewBagAttribute(Type viewBagType)
+        {
+            this.ViewBagType = viewBagType;
+        }
+        public Type ViewBagType { get; }
+    }
+}
