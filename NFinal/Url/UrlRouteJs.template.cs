602200eaf7545d4da88c6f1dede2e805bf3db48c
lucas <348764327@qq.com>
2017-03-16 16:41:17 +0800

    重命名NFinalCore为NFinal

--- NFinal/Url/UrlRouteJs.template.cs
+++ NFinal/Url/UrlRouteJs.template.cs
@@ -6,69 +6,69 @@ using NFinal;
 //http://bbs.nfinal.com
 namespace NFinal.Url
 {
-    [View("/NFinal/Url/UrlRouteJs.cshtml")]
-    public static class UrlRouteJs
+	[View("/NFinal/Url/UrlRouteJs.cshtml")]
+	public static class UrlRouteJs
+	{
+		//如果此处报错，请添加NFinal引用
+		//PMC命令为：Install-Package NFinal
+		public static void Render(NFinal.IO.Writer writer,NFinal.Url.UrlRouteJsModel Model)
+		{
+			writer.Write("");
+			writer.Write("function StringFormat() {\r\n    if (arguments.length == 0)\r\n        return null;\r\n    var str = arguments[0];\r\n    for (var i = 1; i < arguments.length; i++) {\r\n        var re = new RegExp(\'\\\\{\' + (i - 1) + \'\\\\}\', \'gm\');\r\n        str = str.replace(re, arguments[i]);\r\n    }\r\n    return str;\r\n}\r\nvar Url={\r\n");
+bool isFirst = true; 			writer.Write("\r\n");
+foreach (KeyValuePair<Type, Dictionary<string, NFinal.Url.FormatData>> formatController in Model.formatControllerDictionary)
+{
+    string controllerName = formatController.Key.Namespace.Replace('.', '_') + "_" + formatController.Key.Name;
+    isFirst = true;
+			writer.Write("    ");
+			writer.Write("\"");
+			writer.Write(controllerName);
+			writer.Write("\":{\r\n");
+    foreach (KeyValuePair<string, NFinal.Url.FormatData> formatMethod in formatController.Value)
     {
-        //如果此处报错，请添加NFinal引用
-        //PMC命令为：Install-Package NFinal
-        public static void Render(NFinal.IO.Writer writer, NFinal.Url.UrlRouteJsModel Model)
+        if (isFirst)
         {
-            writer.Write("");
-            writer.Write("function StringFormat() {\r\n    if (arguments.length == 0)\r\n        return null;\r\n    var str = arguments[0];\r\n    for (var i = 1; i < arguments.length; i++) {\r\n        var re = new RegExp(\'\\\\{\' + (i - 1) + \'\\\\}\', \'gm\');\r\n        str = str.replace(re, arguments[i]);\r\n    }\r\n    return str;\r\n}\r\nvar Url={\r\n");
-            bool isFirst = true; writer.Write("\r\n");
-            foreach (KeyValuePair<Type, Dictionary<string, NFinal.Url.FormatData>> formatController in Model.formatControllerDictionary)
-            {
-                string controllerName = formatController.Key.Namespace.Replace('.', '_') + "_" + formatController.Key.Name;
-                isFirst = true;
-                writer.Write("    ");
-                writer.Write("\"");
-                writer.Write(controllerName);
-                writer.Write("\":{\r\n");
-                foreach (KeyValuePair<string, NFinal.Url.FormatData> formatMethod in formatController.Value)
-                {
-                    if (isFirst)
-                    {
-                        isFirst = false;
-                    }
-                    else
-                    {
-                        writer.Write("            ");
-                        writer.Write(",\r\n");
-                    }
+            isFirst = false;
+        }
+        else
+        {
+			writer.Write("            ");
+			writer.Write(",\r\n");
+        }
 
-                    if (formatMethod.Value.actionUrlNames != null && formatMethod.Value.actionUrlNames.Length > 0)
-                    {
-                        writer.Write("            ");
-                        writer.Write("\"");
-                        writer.Write(formatMethod.Key);
-                        writer.Write("\":function(");
-                        writer.Write(string.Join(",", formatMethod.Value.actionUrlNames));
-                        writer.Write(")\r\n            ");
-                        writer.Write("{\r\n            ");
-                        writer.Write("return StringFormat(\"");
-                        writer.Write(formatMethod.Value.formatUrl);
-                        writer.Write("\",");
-                        writer.Write(string.Join(",", formatMethod.Value.actionUrlNames));
-                        writer.Write(");\r\n            ");
-                        writer.Write("}\r\n");
-                    }
-                    else
-                    {
-                        writer.Write("            ");
-                        writer.Write("\"");
-                        writer.Write(formatMethod.Key);
-                        writer.Write("\":function()\r\n            ");
-                        writer.Write("{\r\n            ");
-                        writer.Write("return \"");
-                        writer.Write(formatMethod.Value.formatUrl);
-                        writer.Write("\";\r\n            ");
-                        writer.Write("}\r\n");
-                    }
-                }
-                writer.Write("    ");
-                writer.Write("}\r\n");
-            }
-            writer.Write("};");
+        if (formatMethod.Value.actionUrlNames != null && formatMethod.Value.actionUrlNames.Length > 0)
+        {
+			writer.Write("            ");
+			writer.Write("\"");
+			writer.Write(formatMethod.Key);
+			writer.Write("\":function(");
+			writer.Write(string.Join(",", formatMethod.Value.actionUrlNames));
+			writer.Write(")\r\n            ");
+			writer.Write("{\r\n            ");
+			writer.Write("return StringFormat(\"");
+			writer.Write(formatMethod.Value.formatUrl);
+			writer.Write("\",");
+			writer.Write(string.Join(",", formatMethod.Value.actionUrlNames));
+			writer.Write(");\r\n            ");
+			writer.Write("}\r\n");
+        }
+        else
+        {
+			writer.Write("            ");
+			writer.Write("\"");
+			writer.Write(formatMethod.Key);
+			writer.Write("\":function()\r\n            ");
+			writer.Write("{\r\n            ");
+			writer.Write("return \"");
+			writer.Write(formatMethod.Value.formatUrl);
+			writer.Write("\";\r\n            ");
+			writer.Write("}\r\n");
         }
     }
+			writer.Write("    ");
+			writer.Write("}\r\n");
+}
+			writer.Write("};");
+		}
+	}
 }
---
+++ NFinalbb/Cache/MemoryCache.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+#if (NETCORE || CORE)
+using Microsoft.Extensions.Caching.Memory;
+#endif
+namespace NFinal.Cache
+{
+    /// <summary>
+    /// 内存缓存
+    /// </summary>
+    public class MemoryCache : Cache<string>
+    {
+        //private System.Threading.Timer timer = null;
+        //private System.Timers.Timer timer = null;
+        //public static IDictionary<string, MemoryCacheValue> cacheStore = null;
+        public int minutes;
+#if !(NETCORE || CORE)
+        System.Runtime.Caching.MemoryCache _memoryCache = null;
+        System.Runtime.Caching.CacheItemPolicy _cacheItemPolicy = null;
+#else
+        Microsoft.Extensions.Caching.Memory.MemoryCache _memoryCache = null;
+#endif
+        public MemoryCache(int minutes) : base(minutes)
+        {
+            this.minutes = minutes;
+            if (_memoryCache == null)
+            {
+#if !(NETCORE || CORE)
+                _memoryCache = System.Runtime.Caching.MemoryCache.Default;
+                _cacheItemPolicy = new System.Runtime.Caching.CacheItemPolicy();
+                _cacheItemPolicy.SlidingExpiration = TimeSpan.FromMinutes(this.minutes);
+#else
+                _memoryCache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new Microsoft.Extensions.Caching.Memory.MemoryCacheOptions()
+                {
+                });
+#endif
+            }
+        }
+        public override bool HasKey(string key)
+        {
+#if !(NETCORE || CORE)
+            return _memoryCache.Contains(key);
+#else
+            object obj;
+            return _memoryCache.TryGetValue(key, out obj);
+#endif
+        }
+        public override void Remove(string key)
+        {
+#if !(NETCORE || CORE)
+            _memoryCache.Remove(key);
+#else
+            _memoryCache.Remove(key);
+#endif
+        }
+        public override byte[] Get(string key)
+        {
+#if !(NETCORE || CORE)
+            object obj =_memoryCache.Get(key);
+            if (obj != null)
+            {
+                return (byte[])obj;
+            }
+            else
+            {
+                return null;
+            }
+#else
+            object obj;
+            bool success= _memoryCache.TryGetValue(key, out obj);
+            if (success && obj != null)
+            {
+                return (byte[])obj;
+            }
+            else
+            {
+                return null;
+            }
+#endif
+        }
+        public override void Set(string key, byte[] value, int minutes)
+        {
+#if !(NETCORE || CORE)
+            _memoryCache.Set(key, value, _cacheItemPolicy);
+#else
+            Microsoft.Extensions.Caching.Memory.ICacheEntry cacheEntry= _memoryCache.CreateEntry(key);
+            cacheEntry.SlidingExpiration = TimeSpan.FromMinutes(this.minutes);
+            cacheEntry.Value = value;
+#endif
+        }
+    }
+}
