0de472a691183fa5474cf3a1af3d6b325a3f3114
lucas <348764327@qq.com>
2017-06-12 16:41:21 +0800

    修正过滤器。

--- NFinalServerSample/Code/UserCheck.cs
+++ NFinalServerSample/Code/UserCheck.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NFinal.Action;
 using NFinal.Owin;
 
 namespace NFinalServerSample.Code
@@ -10,9 +11,9 @@ namespace NFinalServerSample.Code
     /// 用户自定义过滤器
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
-    public class UserCheckAttribute : Attribute, NFinal.Filter.IRequestFilter<NFinal.Owin.Request>
+    public class UserCheckAttribute : Attribute, NFinal.Filter.IAuthorizationFilter
     {
-        public bool RequestFilter(Request request)
+        public bool AuthorizationFilter<TContext, TRequest>(AbstractAction<TContext, TRequest> action)
         {
             return true;
         }
