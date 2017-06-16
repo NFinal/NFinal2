3778672207bf69147f7f6468f532defc5f32bf2a
lucas <348764327@qq.com>
2017-06-16 17:53:42 +0800

    添加数据属性。

---
+++ NFinal/Data/ColumnAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFinal.Data
+{
+    /// <summary>
+    /// 列设置
+    /// </summary>
+    public class ColumnAttribute :System.Attribute
+    {
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string ColumnName { get; set; }
+        /// <summary>
+        /// 列属性设置
+        /// </summary>
+        /// <param name="columnName"></param>
+        public ColumnAttribute(string columnName)
+        {
+            this.ColumnName = columnName;
+        }
+    }
+}
---
+++ NFinal/Data/TableAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFinal.Data
+{
+    /// <summary>
+    /// 表设置
+    /// </summary>
+    public class TableAttribute : System.Attribute
+    {
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+        /// <summary>
+        /// 表属性设置
+        /// </summary>
+        /// <param name="tableName"></param>
+        public TableAttribute(string tableName)
+        {
+            this.TableName = tableName;
+        }
+    }
+}
